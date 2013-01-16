7e317634fd5087438ba68efa7b50b9186b34f296
jaabyej <jaabyej@gmail.com>
2013-01-16 11:45:37 +0100

    new design

--- My Random password generator/src/Random_password.py
+++ My Random password generator/src/Random_password.py
@@ -15,16 +15,20 @@ def result():
     chara_set = string.ascii_uppercase
     char_set = string.ascii_lowercase
     num_set = string.digits
-    resultVar.set((''.join(random.sample(chara_set,1)))+''.join(random.sample(char_set,6))+''.join(random.sample(num_set,3)))
+    resultVar.set((''.join(random.sample(chara_set,1)))+''.join(random.sample(char_set,1))+''.join(random.sample(num_set,6)))
 
 mainWindow = tk.Tk()
 mainWindow.title("Password Generator")
-mainWindow.minsize(300, 100)
+mainWindow.minsize(350, 250)
 mainWindow.resizable(width=False, height=False)    
 
+mainWindow.rowconfigure('all', option = 200)
+mainWindow.columnconfigure('all', option = 200)
+mainWindow.rowconfigure('all', weight = 3)
+mainWindow.columnconfigure('all', weight = 3)
 # Create a fraom to hold all the GUI elements
 
-win = tk.Frame(mainWindow, width=300, height=100)
+win = tk.Frame(mainWindow, borderwidth=125, bg = 'darkgreen', width=350, height=250)
 win.pack()
 
 # StringVar is the connection from the GUI to the code in this file
@@ -34,7 +38,8 @@ resultVar.set("")
 
 
 win.pack()
-button1 = tk.Button( win, text = "Generate", command = result).pack(side=tk.TOP)
-textEntry1 = tk.Entry( win,  textvariable=resultVar).pack(side=tk.BOTTOM)
-
+button1 = tk.Button( win, text = "Generate", command = result)
+button1.grid(row=4, column=3 )
+textEntry1 = tk.Entry( win,  textvariable=resultVar)
+textEntry1.grid(row=2, column=3 )
 win.mainloop()
